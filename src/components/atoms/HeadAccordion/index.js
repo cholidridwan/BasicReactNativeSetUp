70f4b7c7e01bb2ecc18a033ecef8ef9249f23263
cholidridwan <cholid.r@.com>
2018-11-26 18:46:59 +0700

    add basic native-base

---
+++ src/components/atoms/HeadAccordion/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, Text, Image} from 'react-native';
+import SvgIcon from '../../utils/SvgIcon';
+
+cons HeadAccordion = ()=> {
+    return(
+        <View style={{width: 100%, 
+            marginHorizontal: 20,}}>
+            <Text>title accordion</Text>    
+        </View>
+    )
+}
+
+export default HeadAccordion;
--- src/containers/HomePage/index.js
+++ src/containers/HomePage/index.js
@@ -1,17 +1,48 @@
 import React from 'react';
-import {View, Text, Image} from 'react-native';
+import {StyleSheet, View, Text, Image} from 'react-native';
 //import svgList from '../../assets/styles/svgList';
 import SvgIcon from '../../utils/SvgIcon';
+import {Container, Header, Body, Left, Right, 
+    Button, Title, Icon, Content} from 'native-base';
 
 const HomePage = ()=> {
+    let {left, body} = styles;
     return(
-        <View>
-            <Text>te</Text>
-            {/* <Image source={require('../../assets/images/png/example2.png')}/> */}
-            {/* {svgList['icoLaunch']} */}
-            <SvgIcon name="icoLaunch" />
-        </View>
+        <Container>
+            <Header>
+                <Left  style={left}>
+                    <Button transparent>
+                        <SvgIcon name="icoLanch"/>
+                        {/* <Image source={require('../../assets/images/png/example2.png')}/>  */}
+                    </Button>
+                </Left>
+                <Body>
+                    <Title>Header</Title>
+                </Body>
+                <Right/>
+            </Header>
+            <Content>
+                <SvgIcon name="icoLanch"/>
+                <Image source={require('../../assets/images/png/example2.png')}/> 
+            </Content>
+        </Container>
+
+        // <View>
+        //     {/* <Image source={require('../../assets/images/png/example2.png')}/> */}
+        //     {/* {svgList['icoLaunch']} */}
+        //     <SvgIcon name="icoLaunch" />
+        // </View>
     )
 } 
+
+const styles = StyleSheet.create({
+    left: {
+        flex: 1
+    },
+    body: {
+        flex: 7
+    }
+})
+
 
 export default HomePage;
